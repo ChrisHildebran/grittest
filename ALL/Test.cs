7a9cd17c372a4168b5ec2fa8a6758ac8df02d8ec
Chris Hildebran <32686889+ChrisHildebran@users.noreply.github.com>
2018-11-26 07:11:58 -0800

    Class cleanup

--- ALL/Test.cs
+++ ALL/Test.cs
@@ -1,102 +1,97 @@
-// JH Kelly 2018
-// Kelly Tools For Revit
-// Last User: Chris Hildebran (CHildebran@jhkelly.com)
-// Last Edit: 04/05/2018 - 1:41 PM
-
-#region Using
-
-using System;
-
-#endregion
-
 namespace Gritv3.GritTest
 {
-	public static class Test
-	{
-		#region Fields
+    using System;
 
-		private static string _nameFirst;
+    public static class Test
+    {
+        #region Constants
 
-		private static string _nameLast;
+        private const string _questionHeader15 = "[1-Not like me at all] [2-Not much like me] [3-Somewhat like me] [4-Mostly like me] [5-Very much like me]";
 
-		private static readonly string _questionBreak = "--------------------------------------------------";
+        private const string _questionHeader51 = "[5-Not like me at all] [4-Not much like me] [3-Somewhat like me] [2-Mostly like me] [1-Very much like me]";
 
-		private static int _questionHeader = 1;
+        #endregion
 
-		private static int _questionsAnswered;
+        #region Fields
 
-		private static double _questionScore;
+        private static string _nameFirst;
 
-		private static double _scoreTotal;
+        private static string _nameLast;
 
-		#endregion
+        private static readonly string _questionBreak = "--------------------------------------------------";
 
-		#region Methods
+        private static int _questionHeader = 1;
 
-		internal static void Main(string[] args)
-		{
-			Console.Write("Enter your first name: ");
-			_nameFirst = Console.ReadLine();
+        private static int _questionsAnswered;
 
-			Console.Write("Enter your last name: ");
-			_nameLast = Console.ReadLine();
-			Console.WriteLine("Hello " + _nameFirst + "!");
-			Console.WriteLine(_questionBreak);
+        private static double _questionScore;
 
-			Console.WriteLine("Initial Values");
-			Console.WriteLine("Score Total: " + _scoreTotal);
-			Console.WriteLine("Questions Answered: " + _questionsAnswered);
-			Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
-			Console.WriteLine(_questionBreak);
+        private static double _scoreTotal;
 
-			string[] questions = new string[10];
+        #endregion
 
-			questions[0] = "New ideas and projects sometimes distract me from previous ones.";
-			questions[1] = "Setbacks don't discourage me. I don't give up easily.";
-			questions[2] = "I often set a goal but later choose to pursue a different one.";
-			questions[3] = "I am a hard worker.";
-			questions[4] = "I have a difficulty maintaining my focus on projects that take more than a few months to complete.";
-			questions[5] = "I finish whatever 1 begin.";
-			questions[6] = "My interests change from year to year.";
-			questions[7] = "I am diligent. I never give up.";
-			questions[8] = "I have been obsessed with a certain idea or project for a  short time but later lost interest.";
-			questions[9] = "I have overcome setbacks to conquer an important challenge.";
+        #region Methods
 
-			foreach(var s in questions)
-			{
-				Console.WriteLine("Question: " + s);
-				Console.WriteLine();
+        internal static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            _nameFirst = Console.ReadLine();
 
-				if(_questionHeader % 2 == 0)
-				{
-					Console.WriteLine("-" + _questionHeader15);
-				}
-				else
-				{
-					Console.WriteLine("-" + _questionHeader51);
-				}
+            Console.Write("Enter your last name: ");
+            _nameLast = Console.ReadLine();
+            Console.WriteLine("Hello " + _nameFirst + "!");
+            Console.WriteLine(_questionBreak);
 
-				_questionHeader++;
+            Console.WriteLine("Initial Values");
+            Console.WriteLine("Score Total: " + _scoreTotal);
+            Console.WriteLine("Questions Answered: " + _questionsAnswered);
+            Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
+            Console.WriteLine(_questionBreak);
 
-				_questionScore = int.Parse(Console.ReadLine());
+            string[] questions = new string[10];
 
-				_scoreTotal += _questionScore;
-				Console.WriteLine("Score Total: " + _scoreTotal);
+            questions[0] = "New ideas and projects sometimes distract me from previous ones.";
+            questions[1] = "Setbacks don't discourage me. I don't give up easily.";
+            questions[2] = "I often set a goal but later choose to pursue a different one.";
+            questions[3] = "I am a hard worker.";
+            questions[4] = "I have a difficulty maintaining my focus on projects that take more than a few months to complete.";
+            questions[5] = "I finish whatever 1 begin.";
+            questions[6] = "My interests change from year to year.";
+            questions[7] = "I am diligent. I never give up.";
+            questions[8] = "I have been obsessed with a certain idea or project for a  short time but later lost interest.";
+            questions[9] = "I have overcome setbacks to conquer an important challenge.";
 
-				_questionsAnswered += 1;
-				Console.WriteLine("Questions Answered: " + _questionsAnswered);
-				Console.WriteLine();
-				Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
-				Console.WriteLine();
+            foreach (var s in questions)
+            {
+                Console.WriteLine("Question #" + (_questionsAnswered + 1) + ": " + s);
+                Console.WriteLine();
 
-				Console.WriteLine(_questionBreak);
-			}
-		}
+                if (_questionHeader % 2 == 0)
+                {
+                    Console.WriteLine("-" + _questionHeader15);
+                }
+                else
+                {
+                    Console.WriteLine("-" + _questionHeader51);
+                }
 
-		#endregion
+                _questionHeader++;
 
-		private const string _questionHeader15 = "[1-Not like me at all] [2-Not much like me] [3-Somewhat like me] [4-Mostly like me] [5-Very much like me]";
+                _questionScore = int.Parse(Console.ReadLine());
 
-		private const string _questionHeader51 = "[5-Not like me at all] [4-Not much like me] [3-Somewhat like me] [2-Mostly like me] [1-Very much like me]";
-	}
+                _scoreTotal += _questionScore;
+                Console.WriteLine("Score Total: " + _scoreTotal);
+
+                _questionsAnswered += 1;
+                Console.WriteLine("Questions Answered: " + _questionsAnswered);
+                Console.WriteLine();
+                Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
+                Console.WriteLine();
+
+                Console.WriteLine(_questionBreak);
+            }
+        }
+
+        #endregion
+    }
 }
