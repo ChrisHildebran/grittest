00f5b374557b2dfba61374c61691a09a4d862011
Chris Hildebran (ZBook) <chrishildebran@outlook.com>
2024-03-29 07:43:53 -0600

    Solution Cleanup

--- ALL/Test.cs
+++ ALL/Test.cs
@@ -1,106 +1,88 @@
-// Solution: GritTest
-// Project: Main
-// File: Test.cs
-// Last User: Chris Hildebran
-// Last Edit: 20181216-7:03 PM
+namespace Main;
 
 using System;
 
-namespace Main
+/// <summary>
+/// </summary>
+public static class Test
 {
-          /// <summary>
-          /// </summary>
-          public static class Test
-          {
-                    #region Methods
 
-                    internal static void Main(string[] args)
-                    {
-                              Console.Write("Enter your first name: ");
-                              _nameFirst = Console.ReadLine();
+    internal static void Main(string[] args)
+    {
+        Console.Write("Enter your first name: ");
+        _nameFirst = Console.ReadLine();
 
-                              Console.Write("Enter your last name: ");
-                              _nameLast = Console.ReadLine();
-                              Console.WriteLine("Hello " + _nameFirst + " " + _nameLast + "!");
-                              Console.WriteLine(_questionBreak);
+        Console.Write("Enter your last name: ");
+        _nameLast = Console.ReadLine();
+        Console.WriteLine("Hello " + _nameFirst + " " + _nameLast + "!");
+        Console.WriteLine(_questionBreak);
 
-                              Console.WriteLine("Initial Values");
-                              Console.WriteLine("Score Total: " + _scoreTotal);
-                              Console.WriteLine("Questions Answered: " + _questionsAnswered);
-                              Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
-                              Console.WriteLine(_questionBreak);
+        Console.WriteLine("Initial Values");
+        Console.WriteLine("Score Total: "        + _scoreTotal);
+        Console.WriteLine("Questions Answered: " + _questionsAnswered);
+        Console.WriteLine("Average so far is "   + _scoreTotal / _questionsAnswered);
+        Console.WriteLine(_questionBreak);
 
-                              var questions = new string[10];
+        var questions = new string[10];
 
-                              questions[0] = "New ideas and projects sometimes distract me from previous ones.";
-                              questions[1] = "Setbacks don't discourage me. I don't give up easily.";
-                              questions[2] = "I often set a goal but later choose to pursue a different one.";
-                              questions[3] = "I am a hard worker.";
-                              questions[4] =
-                                        "I have a difficulty maintaining my focus on projects that take more than a few months to complete.";
-                              questions[5] = "I finish whatever 1 begin.";
-                              questions[6] = "My interests change from year to year.";
-                              questions[7] = "I am diligent. I never give up.";
-                              questions[8] =
-                                        "I have been obsessed with a certain idea or project for a  short time but later lost interest.";
-                              questions[9] = "I have overcome setbacks to conquer an important challenge.";
+        questions[0] = "New ideas and projects sometimes distract me from previous ones.";
+        questions[1] = "Setbacks don't discourage me. I don't give up easily.";
+        questions[2] = "I often set a goal but later choose to pursue a different one.";
+        questions[3] = "I am a hard worker.";
+        questions[4] = "I have a difficulty maintaining my focus on projects that take more than a few months to complete.";
+        questions[5] = "I finish whatever 1 begin.";
+        questions[6] = "My interests change from year to year.";
+        questions[7] = "I am diligent. I never give up.";
+        questions[8] = "I have been obsessed with a certain idea or project for a  short time but later lost interest.";
+        questions[9] = "I have overcome setbacks to conquer an important challenge.";
 
-                              foreach (var s in questions)
-                              {
-                                        Console.WriteLine("Question #" + (_questionsAnswered + 1) + ": " + s);
-                                        Console.WriteLine();
+        foreach (var s in questions)
+        {
+            Console.WriteLine("Question #" + (_questionsAnswered + 1) + ": " + s);
+            Console.WriteLine();
 
-                                        if (_questionHeader % 2 == 0)
-                                                  Console.WriteLine("-" + _questionHeader15);
-                                        else
-                                                  Console.WriteLine("-" + _questionHeader51);
+            if (_questionHeader % 2 == 0)
+            {
+                Console.WriteLine("-" + _questionHeader15);
+            }
+            else
+            {
+                Console.WriteLine("-" + _questionHeader51);
+            }
 
-                                        _questionHeader++;
+            _questionHeader++;
 
-                                        _questionScore = int.Parse(Console.ReadLine());
+            _questionScore = int.Parse(Console.ReadLine());
 
-                                        _scoreTotal += _questionScore;
-                                        Console.WriteLine("Score Total: " + _scoreTotal);
+            _scoreTotal += _questionScore;
+            Console.WriteLine("Score Total: " + _scoreTotal);
 
-                                        _questionsAnswered += 1;
-                                        Console.WriteLine("Questions Answered: " + _questionsAnswered);
-                                        Console.WriteLine();
-                                        Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
-                                        Console.WriteLine();
+            _questionsAnswered += 1;
+            Console.WriteLine("Questions Answered: " + _questionsAnswered);
+            Console.WriteLine();
+            Console.WriteLine("Average so far is " + _scoreTotal / _questionsAnswered);
+            Console.WriteLine();
 
-                                        Console.WriteLine(_questionBreak);
-                              }
-                    }
+            Console.WriteLine(_questionBreak);
+        }
+    }
 
-                    #endregion
+    private static string _nameFirst;
 
-                    #region Constants
+    private static string _nameLast;
 
-                    private const string _questionHeader15 =
-                              "[1-Not like me at all] [2-Not much like me] [3-Somewhat like me] [4-Mostly like me] [5-Very much like me]";
+    private static readonly string _questionBreak = "--------------------------------------------------";
 
-                    private const string _questionHeader51 =
-                              "[5-Not like me at all] [4-Not much like me] [3-Somewhat like me] [2-Mostly like me] [1-Very much like me]";
+    private static int _questionHeader = 1;
 
-                    #endregion
+    private static int _questionsAnswered;
 
-                    #region Fields
+    private static double _questionScore;
 
-                    private static string _nameFirst;
+    private static double _scoreTotal;
 
-                    private static string _nameLast;
+    private const string _questionHeader15 = "[1-Not like me at all] [2-Not much like me] [3-Somewhat like me] [4-Mostly like me] [5-Very much like me]";
 
-                    private static readonly string
-                              _questionBreak = "--------------------------------------------------";
+    private const string _questionHeader51 = "[5-Not like me at all] [4-Not much like me] [3-Somewhat like me] [2-Mostly like me] [1-Very much like me]";
 
-                    private static int _questionHeader = 1;
-
-                    private static int _questionsAnswered;
-
-                    private static double _questionScore;
-
-                    private static double _scoreTotal;
-
-                    #endregion
-          }
 }
